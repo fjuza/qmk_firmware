b2b361f5e3b2b644d9d157ac3f939694a8b95ce1
Freddie Juza <fjuza@redbridge.se>
2023-11-15 21:14:30 +0100

    New keymap with auto shift

--- keyboards/splitkb/aurora/lily58/keymaps/fjuza/rules.mk
+++ keyboards/splitkb/aurora/lily58/keymaps/fjuza/rules.mk
@@ -16,7 +16,7 @@
 # NOTE: These are already enabled by default at the revision level
 #ENCODER_ENABLE = yes
 OLED_ENABLE = yes
-AUTO_SHIFT_ENABLED = yes
+AUTO_SHIFT_ENABLE = yes
 
 # RGB Matrix is enabled at the revision level,
 # while we use the regular RGB underglow for testing
